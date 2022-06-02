4075faf46526ea7c2f03df43f3a8f44b636a7a28
Amy Dutton <amy@ahhacreative.com>
2022-06-02 15:53:36 -0500

    ✨ Created Date Helpers

---
+++ web/src/utils/DateHelpers.test.ts
@@ -0,0 +1,34 @@
+import { formatRelativeDate } from './DateHelpers'
+
+describe('Date Helpers', () => {
+  // Reference: https://codewithhugo.com/mocking-the-current-date-in-jest-tests/
+  jest
+    .spyOn(global.Date, 'now')
+    .mockImplementationOnce(() =>
+      new Date('2022-06-02T13:18:02.631Z').valueOf()
+    )
+  it('Shows minutes ago', () => {
+    const date = '2022-06-02T13:21:02.631Z'
+    expect(formatRelativeDate(date)).toEqual('3m')
+  })
+
+  it('Shows hours ago', () => {
+    const date = '2022-06-02T14:18:02.631Z'
+    expect(formatRelativeDate(date)).toEqual('1h')
+  })
+
+  it('Shows yesterday', () => {
+    const date = '2022-06-01T12:18:02.631Z'
+    expect(formatRelativeDate(date)).toEqual('yesterday')
+  })
+
+  it('Shows days ago', () => {
+    const date = '2022-05-30T12:18:02.631Z'
+    expect(formatRelativeDate(date)).toEqual('3d')
+  })
+
+  it('Shows the date', () => {
+    const date = '2022-03-30T12:18:02.631Z'
+    expect(formatRelativeDate(date)).toEqual('March 30, 2022')
+  })
+})
---
+++ web/src/utils/DateHelpers.ts
@@ -0,0 +1,35 @@
+import {
+  differenceInMinutes,
+  differenceInHours,
+  differenceInDays,
+  format,
+  parseISO,
+} from 'date-fns'
+
+export const formatRelativeDate = (date: string): string => {
+  const now = new Date(Date.now())
+  const past = parseISO(date)
+
+  // minutes ago
+  if (Math.abs(differenceInMinutes(past, now)) < 60) {
+    return `${differenceInMinutes(past, now)}m`
+  }
+
+  // hours ago
+  if (Math.abs(differenceInHours(past, now)) < 24) {
+    return `${Math.abs(differenceInHours(past, now))}h`
+  }
+
+  // yesterday
+  if (Math.abs(differenceInDays(past, now)) < 2) {
+    return `yesterday`
+  }
+
+  // days ago (within the last month)
+  if (Math.abs(differenceInDays(past, now)) < 29) {
+    return `${Math.abs(differenceInDays(past, now))}d`
+  }
+
+  // just display the date
+  return format(past, 'MMMM d, yyyy')
+}
