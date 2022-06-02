f2f667db432f82ca5a6e8f14416fe60f2ea239cc
Amy Dutton <amy@ahhacreative.com>
2022-06-01 20:58:39 -0500

    🤡 Update mocked data

--- web/src/components/AvatarList/AvatarList.mocks.ts
+++ web/src/components/AvatarList/AvatarList.mocks.ts
@@ -4,11 +4,11 @@ export const avatars = [
     alt: 'Lincoln Thorpe',
   },
   {
-    src: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+    src: 'https://i.pravatar.cc/150?u=a042581f4e29026704e',
     alt: 'Danica Mckenzie',
   },
   {
-    src: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+    src: 'https://i.pravatar.cc/150?u=a042581f4e29026704f',
     alt: 'Izabella Fox',
   },
 ]
