f97aca6e43f0cdd89ca7669da032644820e69f66
Amy Dutton <amy@ahhacreative.com>
2022-06-02 15:48:31 -0500

    📕 Updated mocked data in story

--- web/src/components/Avatar/Avatar.stories.tsx
+++ web/src/components/Avatar/Avatar.stories.tsx
@@ -1,11 +1,81 @@
-import { Avatar } from './Avatar'
+import { Avatar, IAvatar } from './Avatar'
 
-const Template = (args) => <Avatar {...args} />
+const Template = (args: IAvatar) => <Avatar {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
-  alt: 'First Name Last Name',
+  alt: 'Amy Dutton',
   src: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
 }
 
-export default { title: 'Components/Avatar', component: Avatar }
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+  argTypes: {
+    alt: {
+      name: 'alt',
+      type: { name: 'string', required: true },
+      description: 'alt text',
+      defaultValue: 'null',
+      control: {
+        type: 'text',
+      },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    src: {
+      name: 'src',
+      type: { name: 'string', required: true },
+      description: 'image source path',
+      defaultValue: 'null',
+      control: {
+        type: 'text',
+      },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    className: {
+      name: 'className',
+      type: { name: 'string', required: false },
+      description: 'CSS classes',
+      defaultValue: 'null',
+      control: {
+        type: 'text',
+      },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    height: {
+      name: 'height',
+      type: { name: 'number', required: false },
+      description: 'avatar height in pixels',
+      defaultValue: 32,
+      control: {
+        type: 'number',
+      },
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 32 },
+      },
+    },
+    width: {
+      name: 'width',
+      type: { name: 'number', required: false },
+      description: 'avatar width in pixels',
+      defaultValue: 32,
+      control: {
+        type: 'number',
+      },
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 32 },
+      },
+    },
+  },
+}
