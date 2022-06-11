365e1131742443d32e60ec73c2e55a368d0a5729
Amy Dutton <amy@ahhacreative.com>
2022-06-11 17:37:18 -0400

    ✅ Fixed broken tests on RecentlyCell

--- web/src/components/Recently/RecentlyCell/RecentlyCell.mock.ts
+++ web/src/components/Recently/RecentlyCell/RecentlyCell.mock.ts
@@ -1,6 +1,46 @@
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
-  recently: {
-    id: 42,
-  },
+  recentQuestions: [
+    {
+      id: 1,
+      answeredBy: {
+        fullName: 'Amy Dutton',
+        username: 'selfteachme',
+        avatar: '',
+        avatarColor: 'PUNCH',
+      },
+      question: 'What?',
+    },
+    {
+      id: 2,
+      answeredBy: {
+        fullName: 'Amy Dutton',
+        username: 'selfteachme',
+        avatar: '',
+        avatarColor: 'PUNCH',
+      },
+      question: 'Why?',
+    },
+    {
+      id: 3,
+      answeredBy: {
+        fullName: 'Amy Dutton',
+        username: 'selfteachme',
+        avatar: '',
+        avatarColor: 'PUNCH',
+      },
+      question: 'How?',
+    },
+    {
+      id: 4,
+      answeredBy: {
+        fullName: 'Amy Dutton',
+        username: 'selfteachme',
+        avatar: '',
+        avatarColor: 'PUNCH',
+      },
+      question: 'When?',
+    },
+  ],
+  take: 4,
 })
--- web/src/components/Recently/RecentlyCell/RecentlyCell.test.tsx
+++ web/src/components/Recently/RecentlyCell/RecentlyCell.test.tsx
@@ -1,14 +1,6 @@
-import { render, screen } from '@redwoodjs/testing/web'
+import { render } from '@redwoodjs/testing/web'
 import { Loading, Empty, Failure, Success } from './RecentlyCell'
 import { standard } from './RecentlyCell.mock'
-
-// TODO: Fix failing tests that are failing
-
-// Generated boilerplate tests do not account for all circumstances
-// and can fail without adjustments, e.g. Float and DateTime types.
-//           Please refer to the RedwoodJS Testing Docs:
-//        https://redwoodjs.com/docs/testing#testing-cells
-// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
 
 describe('RecentlyCell', () => {
   it('renders Loading successfully', () => {
@@ -29,15 +21,14 @@ describe('RecentlyCell', () => {
     }).not.toThrow()
   })
 
-  // When you're ready to test the actual output of your component render
-  // you could test that, for example, certain text is present:
-  //
-  // 1. import { screen } from '@redwoodjs/testing/web'
-  // 2. Add test: expect(screen.getByText('Hello, world')).toBeInTheDocument()
-
   it('renders Success successfully', async () => {
     expect(() => {
-      render(<Success recently={standard().recently} />)
+      render(
+        <Success
+          recentQuestions={standard().recentQuestions}
+          take={standard().take}
+        />
+      )
     }).not.toThrow()
   })
 })
